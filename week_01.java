ad6aca33297f0f80d4e16bf0d1fdec670bdf39a7
Bessem <147958693+BessemHuseydi@users.noreply.github.com>
2024-02-18 23:41:57 +0300

    Add files via upload

---
+++ week_01.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class week_01{
+    //jar disariya veriyor 
+    public static void main (String[] args)
+    {
+        Scanner in  = new Scanner(System.in);
+        System.out.print("isim :");
+        String isim = in.next();
+        System.out.print(isim);
+        
+
+        
+    }
+}
